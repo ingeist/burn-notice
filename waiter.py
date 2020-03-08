2153eff2e7e7a83ba4370daa44efc5ffd16bf6f0
j <jenny.xie6@gmail.com>
2020-03-08 05:34:18 -0700

    Add waiter.py

---
+++ waiter.py
@@ -0,0 +1,26 @@
+import serial
+import numpy as np
+from time import sleep,time
+from crontab import CronTab
+
+port = "/dev/ttyACM0"
+
+
+ser = serial.Serial(port,2400,timeout = 0.050)
+ser.baudrate=9600
+
+#cron = CronTab(user='j')
+#job = cron.new(command='python waiter.py')
+#job.minute.on(0,15,23,30,45)
+
+#cron.write()
+sleepmin = 1 # number of minutes to wait/sleep
+
+while (1==1):
+   
+   raw = ser.readline() # looks like b'0\r\n'
+   string_n = raw.decode()
+   string = string_n.rstrip()
+   # if needed convert to float with flt = float(string) but check to make sure it's not nothing
+   print(string)
+   sleep(sleepmin*60)  # sleeps for number of minutes
